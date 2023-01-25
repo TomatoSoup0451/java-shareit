b6fa06ac8bc29ef83efb250a2e4323ce28ec7db7
ilatovskiydm <idm101@yandex.ru>
2023-01-26 03:08:38 +0500

    ref: clea old files

--- src/main/java/ru/practicum/shareit/exception/IllegalBookingAccessException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class IllegalBookingAccessException extends RuntimeException {
-
-    public IllegalBookingAccessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/validationmarkers/OnCreate.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.shareit.validationmarkers;
-
-public interface OnCreate {
-}
